559468bc2b4117bb56b8a3330c27eaf6213cdac9
rgosm <rgo_sm@hotmail.com>
2023-01-23 00:18:35 -0300

    Desafio Cromai

---
+++ desafio-02.py
@@ -0,0 +1,18 @@
+from os import getpid
+from time import sleep
+import logging
+
+count = 0
+pid = getpid()
+
+with open("pid.txt","w+") as pid_file:
+        print(pid)
+        pid_file.write(str(pid))
+
+while(count < 4):
+    if count < 3:
+        print("2: I am alive")
+        sleep(5)
+    else:
+        print("2: I gonna die now, bye.")      
+    count+=1
